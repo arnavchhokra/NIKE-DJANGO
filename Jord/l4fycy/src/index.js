e24eb79e8d8e1f72cb665578d405328c64d2b744
Arnav Chhokra <arnavchhokra@gmail.com>
2024-01-28 10:19:56 -0800

    Removed old frontend

--- Jord/l4fycy/src/index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import App from './App'
-import Main from './Main'
-
-
-createRoot(document.getElementById('root')).render(<Main />)
